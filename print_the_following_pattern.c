10f6e9c51e4ba1ce071236868f876170c58da7a3
SUTRAYETH HARI YUKTHA NANDA <97426182+21MH1A05C9@users.noreply.github.com>
2022-07-12 22:12:23 +0530

    #include<stdio.h> int main() {     int n,i,j,k;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=i-1;j++)         {             printf(" ");         }         for(k=1;k<=n;k++)         {             if(i==1||i==n||k==1||k==n)             {
                    printf("*");
                }
                else
                {
                    printf(" ");
                }
            }
            printf("
    ");
        }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n,i,j,k;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=n;i>=1;i--)
     {
-        for(j=1;j<=i;j++)
+        for(j=1;j<=i-1;j++)
         {
-            printf("%d",j);
+            printf(" ");
+        }
+        for(k=1;k<=n;k++)
+        {
+            if(i==1||i==n||k==1||k==n)
+            {
+                
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("
 ");
